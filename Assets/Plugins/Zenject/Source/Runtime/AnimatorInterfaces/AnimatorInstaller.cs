cfa6da1fa93f287b96e2e548c45b63cf39287621
Dan398 <Dan398@rambler.ru>
2026-09-24 17:29:21 +0400

    Imported Zenject.

---
+++ Assets/Plugins/Zenject/Source/Runtime/AnimatorInterfaces/AnimatorInstaller.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Zenject
+{
+    #if ZENJECT_ANIMATOR
+    public class AnimatorInstaller : Installer<Animator, AnimatorInstaller>
+    {
+        readonly Animator _animator;
+
+        public AnimatorInstaller(Animator animator)
+        {
+            _animator = animator;
+        }
+
+        public override void InstallBindings()
+        {
+            Container.Bind<AnimatorIkHandlerManager>().FromNewComponentOn(_animator.gameObject);
+            Container.Bind<AnimatorIkHandlerManager>().FromNewComponentOn(_animator.gameObject);
+        }
+    }
+    #endif
+}
